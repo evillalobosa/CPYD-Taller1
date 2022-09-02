2ae709b6ac9a30e0536a2418bc530aa0a1ed87d0
Enrique Villalobos <enrique.lnx@gmail.com>
2022-09-01 20:41:25 -0400

    Uploading the class to work with polynomials

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,20 @@
+#include "Utils.h"
+#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <cstdlib>
 
 
 /**
  * Función que muestra los participantes del grupo
  * @param programa nombre del ejecutable
  */
-void participantes(std::string programa);
+void participantes(std::string programa) {
+	std::cout << std::endl << "=== Taller 01: Integrantes ===";
+	std::cout << std::endl << "- Julio Aceituno Zalaya (jaceituno@utem.cl)";
+	std::cout << std::endl << "- Enrique Villalobos (evillalobos@utem.cl)";
+	std::cout << std::endl << "- Oliver Chacón Álvarez (oliver.chacona@utem.cl)" << std::endl;
+}
+
 
 /**
  * Taller computacional
@@ -16,23 +23,27 @@ void participantes(std::string programa);
  * @return El código de salida del programa
  */
 int main(int argc, char** argv) {
+	if (argc == 3) {
+		// Procede a hacer el calculo si la cantidad de parametros es correcta
+		const std::string polinomio(argv[1]);
+		const double valor = std::atof(argv[2]);
+		std::cout << std::endl << "polinomio (cmd): " << polinomio << std::endl;
+		std::cout << "Valor: " << valor << std::endl;
 
-    /**
-     * Incluir acá la lógica del programa
-     * 
-     */
-    if (argc > 1) {
-        // @TODO programar proyecto
-    } else {
-        // Mostrar los integrantes
-        participantes(argv[0]);
-    }
-    return EXIT_SUCCESS;
-}
+		std::cout << "polinomio (convertido): ";
+		std::vector<Monomio> monomios = Utils::convertir(polinomio);
+		std::vector<Monomio>::size_type i = 0;
+		for (i = 0; i < monomios.size(); i++) {
+			Monomio monomio = monomios[i];
+			std::cout << monomio.GetCoeficiente() << "x^" << monomio.GetGrado() << " ";
+		}
+		std::cout << std::endl;
 
-void participantes(std::string programa) {
-    std::cout << std::endl << "=== Taller " << programa << " ===" << std::endl;
-    std::cout << std::endl << "Juanito"; // Reemplazar por su nombre
-    std::cout << std::endl << "Pedrito"; // Reemplazar por su nombre
-    std::cout << std::endl << "Dieguito" << std::endl; // Reemplazar por su nombre
+		double resultado = Utils::evaluar(monomios, valor);
+		std::cout << "Resultado: " << resultado << std::endl;
+	} else {
+		// Muestra los integrantes
+		participantes(argv[0]);
+	}
+	return EXIT_SUCCESS;
 }
